2ba1cbc5667be8650be6452261ad72cb0ef77739
zillulofficial <zillul.hakim100@gmail.com>
2025-04-04 16:23:43 +0600

    mongodb connection completed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 // const jwt = require('jsonwebtoken')
 // const cookieParser = require('cookie-parser');
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.port || 9000
 const app = express()
@@ -38,6 +38,37 @@ app.use(cors({
 //   }
   
   
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dbx7ywt.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const userCollection = client.db('Tanex_International').collection('user')
+    const menuCollection = client.db('Tanex_International').collection('menu')
+    const orderCollection = client.db('Tanex_International').collection('order')
+    
+
+    
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Tanex International Server running smoothly!')
