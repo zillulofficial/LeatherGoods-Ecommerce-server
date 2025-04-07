1cc11a61f14af47ca8d9f921ff379bda5f65e7da
zillulofficial <zillul.hakim100@gmail.com>
2025-04-07 09:42:02 +0600

    admin checking API secured & order related API created

--- index.js
+++ index.js
@@ -2,42 +2,42 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.port || 5000
 const app = express()
 
 // middlewares
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
-    credentials: true,
-    optionsSuccessStatus: 200
-  }))
-  app.use(express.json())
-  app.use(cookieParser())
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionsSuccessStatus: 200
+}))
+app.use(express.json())
+app.use(cookieParser())
 
 // Custom middlewares
 const cookieOption = {
-    httpOnly: true,
-    sameSite: process.env.NODE_ENV === "production" ? 'none' : "strict",
-    secure: process.env.NODE_ENV === "production" ? true : false
-  }
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV === "production" ? 'none' : "strict",
+  secure: process.env.NODE_ENV === "production" ? true : false
+}
 const verifyToken = (req, res, next) => {
-    const token = req?.cookies?.token
-    // console.log("token in the middleware: ", token);
-    if (!token) {
+  const token = req?.cookies?.token
+  // console.log("token in the middleware: ", token);
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
       return res.status(401).send({ message: "Unauthorized access" })
     }
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-        return res.status(401).send({ message: "Unauthorized access" })
-      }
-      req.user = decoded
-      next()
-    })
-  }
-  
-  
+    req.user = decoded
+    next()
+  })
+}
+
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dbx7ywt.mongodb.net/?appName=Cluster0`;
@@ -57,7 +57,7 @@ async function run() {
     const userCollection = client.db('tanexInternational').collection('user')
     const menuCollection = client.db('tanexInternational').collection('menu')
     const orderCollection = client.db('tanexInternational').collection('order')
-    
+
     // middleware
     // use verify admin after verifying token
     const verifyAdmin = async (req, res, next) => {
@@ -88,8 +88,8 @@ async function run() {
     })
 
     // user related API
-    app.post('/users', async(req, res)=>{
-      const user= req.body
+    app.post('/users', async (req, res) => {
+      const user = req.body
       // insert email if it doesn't exist
       // it can be done in various ways (email unique, upsert, simple checking)
       const query = { email: user.email }
@@ -101,19 +101,33 @@ async function run() {
       console.log(result);
       res.send(result)
     })
-
-    app.get('/users', async(req, res)=>{
-      const result= await userCollection.find().toArray()
+    // check for admin 
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email
+      if (email !== req.user.email) {
+        return res.status(403).send({ message: "Forbidden  access" })
+      }
+      const query = { email: email }
+      const user = await userCollection.findOne(query)
+      let admin = false
+      if (user) {
+        admin = user?.role === 'admin'
+      }
+      res.send({ admin })
+    })
+    // get all the users data
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray()
       res.send(result)
     })
-
+    // delete a single user
     app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await userCollection.deleteOne(query)
       res.send(result)
     })
-    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+    app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id
       const filter = { _id: new ObjectId(id) }
       const updateDoc = {
@@ -122,6 +136,13 @@ async function run() {
         }
       };
       const result = await userCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
+    // Order related API
+    app.post('/orders', async(req, res)=>{
+      const order= req.body
+      const result= await orderCollection.insertOne(order)
       res.send(result)
     })
 
@@ -137,6 +158,6 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Tanex International Server running smoothly!')
-  })
-  app.listen(port, () => console.log(`Server Running at port: ${port}`))
+  res.send('Tanex International Server running smoothly!')
+})
+app.listen(port, () => console.log(`Server Running at port: ${port}`))
