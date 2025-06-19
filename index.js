b7ca1f5e493bbbb3d30504633b7c02ca11737b07
zillulofficial <zillul.hakim100@gmail.com>
2025-06-19 21:49:42 +0600

    purchase related API created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const SSLCommerzPayment = require('sslcommerz-lts')
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -40,7 +41,7 @@ const verifyToken = (req, res, next) => {
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dbx7ywt.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qc4htf2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -51,12 +52,17 @@ const client = new MongoClient(uri, {
   }
 });
 
+const store_id = process.env.STORE_ID
+const store_passwd = process.env.STORE_PASS
+const is_live = false //true for live, false for sandbox
+
 async function run() {
   try {
 
-    const userCollection = client.db('tanexInternational').collection('user')
-    const itemCollection = client.db('tanexInternational').collection('item')
-    const orderCollection = client.db('tanexInternational').collection('order')
+    const userCollection = client.db('leather_Ecommerce').collection('user')
+    const itemCollection = client.db('leather_Ecommerce').collection('item')
+    const orderCollection = client.db('leather_Ecommerce').collection('order')
+    const purchaseCollection = client.db('leather_Ecommerce').collection('purchase')
 
     // middleware
     // use verify admin after verifying token
@@ -99,7 +105,7 @@ async function run() {
         return res.send({ message: "user already exists", insertedId: null })
       }
       const result = await userCollection.insertOne(user)
-      console.log(result);
+
       res.send(result)
     })
     // check for admin 
@@ -128,7 +134,7 @@ async function run() {
       const result = await userCollection.deleteOne(query)
       res.send(result)
     })
-    app.patch('/users/admin/:id',verifyToken, async (req, res) => {
+    app.patch('/users/admin/:id', verifyToken, async (req, res) => {
       const id = req.params.id
       const filter = { _id: new ObjectId(id) }
       const updateDoc = {
@@ -160,6 +166,115 @@ async function run() {
       res.send(result)
     })
 
+    // purchase related API
+    const tran_id = new ObjectId().toString();
+
+    app.post("/purchase", async (req, res) => {
+      const defaultData = req?.body
+      const product = await itemCollection.findOne({
+        _id: new ObjectId(req.body?.productId),
+      });
+
+      const mongoPurchaseData = {
+        ...product,
+        buyerName: defaultData?.buyerName,
+        buyerPhone: defaultData?.buyerPhone,
+        buyerEmail: defaultData?.buyerEmail,
+        buyerAddress: defaultData?.buyerAddress,
+      }
+      const data = {
+        total_amount: defaultData?.price,
+        currency: defaultData?.currency,
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: `http://localhost:5000/payment/success/${tran_id}`,
+        fail_url: `http://localhost:5000/payment/fail/${tran_id}`,
+        cancel_url: "http://localhost:3030/cancel",
+        ipn_url: "http://localhost:3030/ipn",
+        shipping_method: "Courier",
+        product_name: product?.name,
+        product_category: "Electronic",
+        product_profile: "general",
+        cus_name: defaultData?.buyerName,
+        cus_email: defaultData?.buyerEmail,
+        cus_add1: defaultData?.buyerAddress,
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: defaultData?.buyerPhone,
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+      };
+
+      // console.log(data);
+
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+      sslcz.init(data).then((apiResponse) => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+        res.send({ url: GatewayPageURL });
+
+        const finalPurchase = {
+          mongoPurchaseData,
+          paidStatus: false,
+          transactionId: tran_id,
+        };
+        const result = purchaseCollection.insertOne(finalPurchase);
+
+        console.log("Redirecting to: ", GatewayPageURL);
+      });
+
+      app.post("/payment/success/:tranId", async (req, res) => {
+        // console.log(req.params.tranId);
+        const result = await purchaseCollection.updateOne(
+          { transactionId: req.params?.tranId },
+          {
+            $set: {
+              paidStatus: true,
+            },
+          }
+        );
+
+        if (result.modifiedCount > 0) {
+          res.redirect(
+            `http://localhost:5173/payment/success/${req.params?.tranId}`
+          );
+        }
+      });
+
+      app.post("/payment/fail/:tranId", async (req, res) => {
+        const result = await purchaseCollection.deleteOne({
+          transactionId: req.params?.tranId,
+        });
+
+        if (result.deletedCount) {
+          res.redirect(
+            `http://localhost:5173/payment/failiure/${req.params?.tranId}`
+          );
+        }
+      });
+
+    })
+    // get all purchase data
+    app.get('/purchase', async(req, res)=>{
+      const result= await purchaseCollection.find().toArray()
+      res.send(result)
+    })
+    // delete one purchase
+    app.delete('/purchase/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await purchaseCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
     // Items Related API
     // add an items
@@ -180,7 +295,7 @@ async function run() {
       res.send(result)
     })
     // delete a single item
-    app.delete('/items/:id', verifyAdmin, async (req, res) => {
+    app.delete('/items/:id', async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await itemCollection.deleteOne(query)
